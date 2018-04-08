3b283c39200f1ea63f021c1e79e5439a94237b5b
Ibrahim Khaled <ikhaled@ulb.ac.be>
2018-04-08 16:38:36 +0200

    Macro
    code pour récuperer les indices des évènements de sigma<2s

--- Macro.C
+++ Macro.C
@@ -1,14 +1,18 @@
 //Import of the histogramme hAllSkyCoarse_sigma;1
 #include <TH2.h>
 void Macro(){
-TH2D *histo = DATA.Get("hAllSkyCoarse_sigma;1")
+TFile DATA("DATA.root");
+TH2D *histo = DATA.Get("hAllSkyCoarse_sigma;1");
+histo->Draw("colz");
 //Searching of maximum bins on x and y axes
-double nbinx = histo -> GetNbinsX()
-double nbiny = histo -> GetNbinsy()
+double nbinx = histo -> GetNbinsX();
+double nbiny = histo -> GetNbinsY();
 //Searching the index where the content of the bins respect the condition
-for (int i = 0, i < nbinx, i++){
-  for (int j = 0, j < nbiny, j++){
-    Float_t content = histo -> GetBincontent(i,j)
+for (int i = 0; i < nbinx; i++);
+{
+  for (int j = 0; j < nbiny; j++);
+  {
+    Float_t content = histo -> GetBincontent(i,j);
     //content is the date of the event in julien day and the condition is
     //that content<log(2 seconds in Julien days)
     if(content < -7.198073971421246 ){
